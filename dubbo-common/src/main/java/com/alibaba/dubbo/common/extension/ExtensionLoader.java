9221b05814ee230b8b75b09f2f887459ead40004
jaychang <jaychang1987@gmail.com>
2022-08-18 11:03:52 +0800

    feat: ExtensionLoader增加注释说明实现类对象经多层包装的实现

--- dubbo-common/src/main/java/com/alibaba/dubbo/common/extension/ExtensionLoader.java
+++ dubbo-common/src/main/java/com/alibaba/dubbo/common/extension/ExtensionLoader.java
@@ -503,6 +503,8 @@ public class ExtensionLoader<T> {
             }
             injectExtension(instance);
             Set<Class<?>> wrapperClasses = cachedWrapperClasses;
+            // 如果wrapperClasses不为空，instance会经过wrapperClass的包装,如果是有多个，那么经过多层包装
+            // instance = injectExtension((T) wrapperClass.getConstructor(type).newInstance(instance)); 写的非常棒，实现多层包装
             if (wrapperClasses != null && wrapperClasses.size() > 0) {
                 for (Class<?> wrapperClass : wrapperClasses) {
                     instance = injectExtension((T) wrapperClass.getConstructor(type).newInstance(instance));
